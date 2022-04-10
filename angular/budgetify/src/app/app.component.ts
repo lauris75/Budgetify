b950ec586b23c3fcaffcdafddf565f285dbcdfb3
Laurynas Plokštys <laurynas.plok5@gmail.com>
2022-04-11 00:14:18 +0300

    Angular homework 2
    Adding login page and logic, guards and not found (404) exception

--- angular/budgetify/src/app/app.component.ts
+++ angular/budgetify/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   title = 'budgetify';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  healthCheck() {
+    this.authService.healthCheck().subscribe((data) => console.log(data));
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { name: user.name, sub: user.id};
+        const payload = { name: user._doc.name, sub: user._doc._id};
         return {
             access_token: this.jwtService.sign(payload)
         }
